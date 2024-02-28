a4d1d9d29535d45801078885a84a907f1daafb0f
chinazam C. Dennar <exchangehub@aol.com>
2024-02-28 15:05:49 -0700

    royal commit

--- cicd_start_stop/backend.tf
+++ cicd_start_stop/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     organization = "the_hub"
     workspaces {
-      name = "terraform_workspace"
+      name = "golden_workspace"
     }
   }
   required_providers {
