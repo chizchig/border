ad801960d2c303094f8f54760b92ace6f213d806
Chinazam C. Dennar <140847987+chizchig@users.noreply.github.com>
2024-03-12 01:59:27 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,14 @@
 terraform {
-  # backend "remote" {
-   # organization = "the_hub"
-   # workspaces {
-     # name = "border"
-   # }
-  # }
-  required_providers  
+  backend "remote" {
+    organization = "the_hub"
+    workspaces {
+      name = "border"
+    }
+    # Token for authentication
+    hostname     = "app.terraform.io"
+    token        = "kCz49yyin48gHg.atlasv1.sBcstTVraAFuhYF4tJjSfbspqsVYCetWPQDbyKh54dA6J5FTal0XJVioU485T5s6vjc"
+  }
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.35.0"
