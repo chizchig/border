3a9b18fb84784858c2c218452d523e98a1fe207f
chinazam C. Dennar <exchangehub@aol.com>
2024-03-12 03:18:51 -0600

    g commit

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 terraform {
-  backend "remote" {
-    organization = "the_hub"
-    workspaces {
-      name = "border"
-    }
-    # Token for authentication
-    hostname     = "app.terraform.io"
-    token        = "kCz49yyin48gHg.atlasv1.sBcstTVraAFuhYF4tJjSfbspqsVYCetWPQDbyKh54dA6J5FTal0XJVioU485T5s6vjc"
-  }
+  # backend "remote" {
+  #   organization = "the_hub"
+  #   workspaces {
+  #     name = "border"
+  #   }
+  #   # Token for authentication
+  #   hostname     = "app.terraform.io"
+  #   token        = "gz6WHk9kEDZdng.atlasv1.6E7FyYfi1EX7K1ZVO3mjuFn1YmG4pCAucKTOJLB8Lx3Lk0LPF44GteIQMrGatRK3ZhM"
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
